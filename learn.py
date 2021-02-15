cd8891dfdcc12a7352dc7fd645f369e9af08e238
Levvw <69399787+Levvw@users.noreply.github.com>
2021-02-15 18:16:20 +0200

    after implementing add, delete and save functions
    note that the program used for SQLite is DB browser

--- learn.py
+++ learn.py
@@ -1,3 +1,45 @@
+import sqlite3
+db = sqlite3.connect("E:\Bodi\Python\skills.db")
+cr = db.cursor()
+uid = 1
+# string formating isnt good for queries to prevent SQL injection
+# but im doing it anyways for this one project
+cr.execute(
+    "create table if not exists skills (name text, progress text, user_id integer)")
+
+
+def save_and_close():
+    db.commit()
+    db.close()
+    print("DataBase connection is saved and closed")
+
+
+def show_skills():
+
+    save_and_close()
+
+
+def add_skills():
+    sk = input("Add a new Skill").strip().capitalize()
+    prog = input(" write the progress of the skill").strip()
+    cr.execute(
+        f"insert into skills(name, progress, user_id) values('{sk}','{prog}','{uid}')")
+    save_and_close()
+
+
+def delete_skills():
+    sk = input("Enter the skill name : ").strip().capitalize()
+    cr.execute(
+        f"delete from skills where name = '{sk}' and user_id = '{uid}' ")
+
+    save_and_close()
+
+
+def update_skills():
+
+    save_and_close()
+
+
 input_message = """
  What do u want to do?
 's' => Show all skills
@@ -10,7 +52,6 @@ Choose Option:
 user_input = input(input_message).strip().lower()
 commands_list = ['s', 'a', 'd', 'u', 'q']
 if user_input in commands_list:
-    print(f"Command Entered {user_input}")
     if user_input == 's':
         show_skills()
     elif user_input == 'a':
@@ -24,19 +65,3 @@ if user_input in commands_list:
 
 else:
     print(f"sorry {user_input} command is not found")
-
-
-def show_skills():
-    pass
-
-
-def add_skills():
-    pass
-
-
-def delete_skills():
-    pass
-
-
-def update_skills():
-    pass
